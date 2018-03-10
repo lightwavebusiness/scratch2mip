1a43b05fe86fda274096ffa8d43245f5b340066b
lightwavebusiness <leon@l-w-b.co>
2018-03-10 17:29:39 +0000

    Update scratch2mip.js

--- scratch2mip.js
+++ scratch2mip.js
@@ -95,20 +95,8 @@
       return radar;
     }
 
-    var lang = ((navigator.language || navigator.userLanguage) == 'ja') ? 'ja' : 'en';
+    var lang = 'en';
     var locale = {
-        ja: {
-            connect: '接続する',
-            turn_right: '右に %n 度回す',
-            turn_left: '左に %n 度回す',
-            move_forward: '%n 歩前進させる',
-            move_backward: '%n 歩後退させる',
-            set_radar_on: 'レーダーをオンにする',
-            when_clear: '障害物がないとき',
-            when_far: '障害物が遠いとき',
-            when_near: '障害物が近いとき',
-            get_radar: '障害物との距離(1:なし 2:遠い 3:近い)'
-        },
         en: {
             connect: 'connect',
             turn_right: 'turn right %n degrees',
@@ -119,7 +107,8 @@
             when_clear: 'when clear',
             when_far: 'when far',
             when_near: 'when near',
-            get_radar: 'get radar(1:clear 2:far 3:near)'
+            get_radar: 'get radar(1:clear 2:far 3:near)',
+            punch: 'punch',
         },
     }
 
@@ -135,6 +124,7 @@
             ['h', 'MiP: ' + locale[lang].when_far, 'when_far'],
             ['h', 'MiP: ' + locale[lang].when_near, 'when_near'],
             ['r', 'MiP: ' + locale[lang].get_radar, 'get_radar']
+            ['p', 'MiP: ' + locale[lang].punch, 'punch']
         ],
         menus: {
             // radar_mode: ['radar', 'gesture']
@@ -142,5 +132,5 @@
         }
     };
 
-    ScratchExtensions.register('Scratch2MiP', descriptor, ext);
+    ScratchExtensions.register('Scratch2MiP-Leon', descriptor, ext);
 })({});
